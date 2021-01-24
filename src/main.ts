6440c6aecf2f3e0cf47d5287f1f1cea1f7602c5f
Bloodsfire <voobsheeee@mail.ru>
2021-01-24 13:22:45 +0600

    Create app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+import { AppConfigService } from './config/app/config.service';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const appConfig: AppConfigService = app.get('AppConfigService');
+
+  await app.listen(appConfig.port);
 }
 bootstrap();
